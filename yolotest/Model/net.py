53a1cfbf1f8281a862ad7e6b21263ef9291e6307
cvYouTian <youclark00@gmail.com>
2023-11-04 11:29:08 +0800

    modified net

--- yolotest/Model/net.py
+++ yolotest/Model/net.py
@@ -9,6 +9,7 @@ def auto_padding(kernel_size, pad=None):
         pad = kernel_size // 2 if isinstance(kernel_size, int) else (x // 2 for x in kernel_size)
     return pad
 
+
 class Conv(nn.Module):
     def __init__(self, input_chanel,
                  output_chanel,
@@ -33,6 +34,7 @@ class SPPF(nn.Module):
         super(SPPF, self).__init__()
         self.conv = Conv(in_channel, out_channel, (1, 1), 1, padding=auto_padding(1, None))
         self.maxpooling = nn.MaxPool2d(kernel_size=5, stride=1, padding=auto_padding(5, None))
+
     def forward(self, x):
         x0 = self.conv(x)
         x1 = self.maxpooling(x0)
@@ -52,11 +54,11 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
         self.shortcut = shortcut
         self.conv1 = Conv(input_chanel=in_channel,
-                          output_chanel=int(out_channel*0.5),
+                          output_chanel=int(out_channel * 0.5),
                           kernel_size=kernel_size,
                           stride=1,
                           padding=auto_padding(kernel_size=kernel_size))
-        self.conv2 = Conv(input_chanel=int(out_channel*0.5),
+        self.conv2 = Conv(input_chanel=int(out_channel * 0.5),
                           output_chanel=out_channel,
                           kernel_size=kernel_size,
                           stride=1,
@@ -74,20 +76,20 @@ class Bottleneck(nn.Module):
 
 
 class C2f(nn.Module):
-    def __init__(self,shortcut: bool, block: int, in_channel, out_channel):
+    def __init__(self, shortcut: bool, block: int, in_channel, out_channel):
         super(C2f, self).__init__()
         self.conv1 = Conv(input_chanel=in_channel,
                           output_chanel=out_channel,
                           kernel_size=1,
                           stride=1,
                           padding=auto_padding(kernel_size=1, pad=None))
-        self.conv2 = Conv(input_chanel=int(out_channel*0.5*3),
+        self.conv2 = Conv(input_chanel=int(out_channel * 0.5 * 3),
                           output_chanel=out_channel,
                           kernel_size=1,
                           stride=1,
                           padding=auto_padding(kernel_size=1, pad=None))
         self.bottleneck = nn.ModuleList(
-            [Bottleneck(int(out_channel*0.5), int(out_channel*0.5), shortcut) for _ in range(block)])
+            [Bottleneck(int(out_channel * 0.5), int(out_channel * 0.5), shortcut) for _ in range(block)])
 
     def forward(self, x):
         module = list(self.conv1(x).chunk(2, 1))
@@ -101,23 +103,24 @@ class C2f(nn.Module):
         # x3 = torch.cat(x2, dim=1)
         #
 
+
 class YOLOv8l(nn.Module):
     def __init__(self):
         super(YOLOv8l, self).__init__()
         # backbone
-        self.conv_0 = Conv(3, 64,3, 2,
-                          padding=auto_padding(kernel_size=3, pad=None)),
+        self.conv_0 = Conv(3, 64, 3, 2,
+                           padding=auto_padding(kernel_size=3, pad=None)),
         self.conv_1 = Conv(64, 128, 3, 2,
-                          padding=auto_padding(kernel_size=3, pad=None)),
+                           padding=auto_padding(kernel_size=3, pad=None)),
         self.c2f_2 = C2f(True, 3, 128, 128),
         self.conv_3 = Conv(128, 256, 3, 2,
-                          padding=auto_padding(kernel_size=3, pad=None)),
+                           padding=auto_padding(kernel_size=3, pad=None)),
         self.c2f_4 = C2f(True, 6, 256, 256),
         self.conv_5 = Conv(256, 512, 3, 2,
-                          padding=auto_padding(kernel_size=3, pad=None)),
+                           padding=auto_padding(kernel_size=3, pad=None)),
         self.c2f_6 = C2f(True, 6, 512, 512),
         self.conv_7 = Conv(512, 512, 3, 2,
-                          padding=auto_padding(kernel_size=3, pad=None)),
+                           padding=auto_padding(kernel_size=3, pad=None)),
         self.c2f_8 = C2f(True, 3, 512, 512),
         self.sppf_9 = SPPF(512, 512)
 
@@ -126,10 +129,10 @@ class YOLOv8l(nn.Module):
         self.c2f_12 = C2f(False, 3, 1024, 512)
         self.c2f_15 = C2f(False, 3, 512, 512)
         self.conv_16 = Conv(256, 256, 3, 2,
-                           padding=auto_padding(3, None))
+                            padding=auto_padding(3, None))
         self.c2f_18 = C2f(False, 3, 512, 512)
         self.conv_19 = Conv(512, 512, 3, 2,
-                           padding=auto_padding(3, None))
+                            padding=auto_padding(3, None))
         self.c2f_21 = C2f(False, 3, 1024, 512)
 
     def forward(self, x):
@@ -162,10 +165,5 @@ class YOLOv8l(nn.Module):
         return x15, x18, x21
 
 
-
-
-
 net = YOLOv8l()
 print(net)
-
-
