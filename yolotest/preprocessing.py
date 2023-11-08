a5b05c7a8642e6738cf7646100cfdccec37def54
cvYouTian <youclark00@gmail.com>
2023-11-08 18:43:26 +0800

    modified preprpcessing.py

--- yolotest/preprocessing.py
+++ yolotest/preprocessing.py
@@ -1,5 +1,8 @@
 import os
+from pathlib import Path
+from torchvision.transforms import Compose
 from PIL import Image
+from typing import Union
 from torch import nn
 import torch.nn.functional as F
 import torchvision
@@ -7,8 +10,15 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms, datasets
 
 class HSTS6(Dataset):
-    def __init__(self, root_dir, image_dir, label_dir, transform):
+    def __init__(self,
+                 size: int,
+                 root_dir:Union[Path, str]= "",
+                 image_dir:Union[Path, str]="",
+                 label_dir:Union[Path, str]="",
+                 augment: bool=False):
         super(HSTS6, self).__init__()
+        self.size = size
+        self.augment = augment
         self.root_dir = root_dir
         self.image_dir = image_dir
         self.label_dir = label_dir
@@ -16,9 +26,21 @@ class HSTS6(Dataset):
         self.label_path = os.path.join(self.root_dir, self.label_dir)
         self.images = os.listdir(self.image_path)
         self.labels = os.listdir(self.label_path)
-        self.transform = transform
+        self.transform = self.build_transform(self.size)
         self.images.sort()
         self.labels.sort()
+
+    def build_transform(self):
+        """
+
+        """
+        if self.augment:
+            Compose([
+                transforms.Resize((self.size, self.size)),
+                transforms.ToTensor()
+            ])
+        else:
+            Compose([])
 
     def __getitem__(self, item):
         image_item = self.images[item]
@@ -40,12 +62,8 @@ class HSTS6(Dataset):
 
 
 if __name__ == '__main__':
-    root_dir = ""
-    image_dir = ""
-    label_dir = ""
-    transforms = torchvision.transforms.Compose([
-        transforms.Resize((640, 640)),
-        transforms.ToTensor()
-    ])
-    mydata = HSTS6()
-    test_set = DataLoader(dataset=HSTS6, batch_size=4, shuffle=True, num_workers=4, drop_last=False)
+    root_dir = os.path.join("/home/youtian/Documents/pro/pyCode/datasets/HSTS6")
+    image_dir = os.path.join("images")
+    label_dir = os.path.join("labels")
+    hsts = HSTS6(root_dir, image_dir, label_dir, 640, )
+    test_set = DataLoader(dataset=hsts, batch_size=4, shuffle=True, num_workers=4, drop_last=False)
