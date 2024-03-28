5e885c6dfdcb34160280292ab13cafeb72889701
cvYouTian <youclark00@gmail.com>
2024-03-28 19:56:50 +0800

    firstly add asffquadruplevel function

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -119,6 +119,81 @@ class AsffTribeLevel(nn.Module):
         return out
 
 
+class AsffQuadrupLevel(nn.Module):
+    def __init__(self, level):
+        super(AsffQuadrupLevel, self).__init__()
+        self.level = level
+        self.dim = [512, 512, 256, 256]
+        self.inter_dim = self.dim[self.level]
+        # 0 是最深的feature
+        if level == 0:
+            self.stride_level_1 = nn.MaxPool2d(kernel_size=2, stride=2)
+            self.stride_level_2 = add_conv(256, self.inter_dim, 3, 2)
+            self.expand = add_conv(self.inter_dim, 512, 3, 1)
+        if level == 1:
+            self.stride_level_1 = nn.MaxPool2d(kernel_size=2, stride=2)
+            self.stride_level_2 = add_conv(256, self.inter_dim, 3, 2)
+            self.expand = add_conv(self.inter_dim, 512, 3, 1)
+        elif level == 2:
+            self.stride_level_2 = add_conv(256, self.inter_dim, 3, 2)
+            self.expand = add_conv(self.inter_dim, 512, 3, 1)
+        elif level == 3:
+            self.compress_level_0 = add_conv(512, self.inter_dim, 1, 1)
+            self.compress_level_1 = add_conv(512, self.inter_dim, 1, 1)
+            self.expand = add_conv(self.inter_dim, 256, 3, 1)
+
+        compress_c = 8
+
+        self.weight_level_0 = add_conv(self.inter_dim, compress_c, 1, 1)
+        self.weight_level_1 = add_conv(self.inter_dim, compress_c, 1, 1)
+        self.weight_level_2 = add_conv(self.inter_dim, compress_c, 1, 1)
+        self.weight_level_3 = add_conv(self.inter_dim, compress_c, 1, 1)
+
+        self.weight_levels = nn.Conv2d(compress_c * 4, 4, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        if self.level == 0:
+            level_0_resized = x[0]
+            level_1_resized = self.stride_level_1(x[1])
+            level_2_downsampled_inter = F.max_pool2d(x[2], 3, stride=2, padding=1)
+            level_2_resized = self.stride_level_2(level_2_downsampled_inter)
+
+        elif self.level == 1:
+            level_0_resized = F.interpolate(x[0], scale_factor=2, mode='nearest')
+            level_1_resized = x[1]
+            level_2_resized = self.stride_level_2(x[2])
+
+        elif self.level == 2:
+            level_0_compressed = self.compress_level_0(x[0])
+            level_0_resized = F.interpolate(level_0_compressed, scale_factor=4, mode='nearest')
+            level_1_compressed = self.compress_level_1(x[1])
+            level_1_resized = F.interpolate(level_1_compressed, scale_factor=2, mode='nearest')
+            level_2_resized = x[2]
+
+        elif self.level == 3:
+            level_0_compressed = self.compress_level_0(x[0])
+            level_0_resized = F.interpolate(level_0_compressed, scale_factor=4, mode='nearest')
+            level_1_compressed = self.compress_level_1(x[1])
+            level_1_resized = F.interpolate(level_1_compressed, scale_factor=2, mode='nearest')
+            level_2_resized = x[2]
+
+        level_0_weight_v = self.weight_level_0(level_0_resized)
+        level_1_weight_v = self.weight_level_1(level_1_resized)
+        level_2_weight_v = self.weight_level_2(level_2_resized)
+        level_3_weight_v = self.weight_level_3(level_3_resized)
+        levels_weight_v = torch.cat((level_0_weight_v, level_1_weight_v, level_2_weight_v), 1)
+        levels_weight = self.weight_levels(levels_weight_v)
+        levels_weight = F.softmax(levels_weight, dim=1)
+
+        fused_out_reduced = level_0_resized * levels_weight[:, 0:1, :, :] + \
+                            level_1_resized * levels_weight[:, 1:2, :, :] + \
+                            level_2_resized * levels_weight[:, 2:, :, :]
+
+        out = self.expand(fused_out_reduced)
+
+        return out
+
+
 class AsffDoubLevel(nn.Module):
     def __init__(self, level):
         super(AsffDoubLevel, self).__init__()
