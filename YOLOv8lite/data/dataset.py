f1987edd84cc7058a50911b57b53a5b7e84203ce
cvyoutian <youclark00@gmail.com>
2025-04-07 14:34:58 +0800

    modified train.py

---
+++ YOLOv8lite/data/dataset.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from typing import Union
+
+
+def check_det_dataset(dataset):
+    data = check_file(dataset)
+
+def check_file(dataset):
+    file = str(file).strip()
+
+if __name__ == '__main__':
+    pass
